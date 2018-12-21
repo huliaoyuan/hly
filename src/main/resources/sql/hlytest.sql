1321993b8f81a868960263b5406d9cfe6b8732ad
huliaoyuan <785976101@qq.com>
2018-12-21 17:20:38 +0800

    sql文件修改

--- src/main/resources/sql/hlytest.sql
+++ src/main/resources/sql/hlytest.sql
@@ -10,16 +10,16 @@ Target Server Type    : MYSQL
 Target Server Version : 50556
 File Encoding         : 65001
 
-Date: 2018-12-17 15:21:26
+Date: 2018-12-21 17:20:18
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for t_class
+-- Table structure for class
 -- ----------------------------
-DROP TABLE IF EXISTS `t_class`;
-CREATE TABLE `t_class` (
+DROP TABLE IF EXISTS `class`;
+CREATE TABLE `class` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `class` varchar(255) DEFAULT NULL,
@@ -29,45 +29,139 @@ CREATE TABLE `t_class` (
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of t_class
+-- Records of class
 -- ----------------------------
-INSERT INTO `t_class` VALUES ('11', '三年二班', '21321', '2');
+INSERT INTO `class` VALUES ('11', '三年二班', '21321', '2');
 
 -- ----------------------------
--- Table structure for t_user
+-- Table structure for permission
 -- ----------------------------
-DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user` (
-  `userid` varchar(50) NOT NULL,
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
+  `id` varchar(50) NOT NULL,
+  `permission` varchar(255) DEFAULT NULL,
+  `permissionname` varchar(255) DEFAULT NULL,
+  `text1` varchar(255) DEFAULT NULL,
+  `text2` varchar(255) DEFAULT NULL,
+  `text3` varchar(255) DEFAULT NULL,
+  `text4` varchar(255) DEFAULT NULL,
+  `text5` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
+INSERT INTO `permission` VALUES ('1', 'view', 'view', null, null, null, null, null);
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` varchar(50) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `rolename` varchar(255) DEFAULT NULL,
+  `text1` varchar(255) DEFAULT NULL,
+  `text2` varchar(255) DEFAULT NULL,
+  `text3` varchar(255) DEFAULT NULL,
+  `text4` varchar(255) DEFAULT NULL,
+  `text5` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('1', 'ordinary', 'ordinary', '', null, null, null, null);
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission` (
+  `id` varchar(50) NOT NULL,
+  `permissionid` varchar(255) DEFAULT NULL,
+  `roleid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role_permission
+-- ----------------------------
+INSERT INTO `role_permission` VALUES ('1', '1', '1');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` varchar(50) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `classid` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`userid`)
+  `text1` varchar(255) DEFAULT NULL,
+  `text2` varchar(255) DEFAULT NULL,
+  `text3` varchar(255) DEFAULT NULL,
+  `text4` varchar(255) DEFAULT NULL,
+  `text5` varchar(255) DEFAULT NULL,
+  `text6` varchar(255) DEFAULT NULL,
+  `text7` varchar(255) DEFAULT NULL,
+  `text8` varchar(255) DEFAULT NULL,
+  `text9` varchar(255) DEFAULT NULL,
+  `text10` varchar(255) DEFAULT NULL,
+  `text11` varchar(255) DEFAULT NULL,
+  `text12` varchar(255) DEFAULT NULL,
+  `text13` varchar(255) DEFAULT NULL,
+  `text14` varchar(255) DEFAULT NULL,
+  `text15` varchar(255) DEFAULT NULL,
+  `text16` varchar(255) DEFAULT NULL,
+  `text17` varchar(255) DEFAULT NULL,
+  `text18` varchar(255) DEFAULT NULL,
+  `text19` varchar(255) DEFAULT NULL,
+  `text20` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of t_user
+-- Records of user
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('1c8798f464df4cdba31504db95ce4e9d', '盖伦啊啊啊1', '80082088201', '1', '11');
-INSERT INTO `t_user` VALUES ('2', '盖伦', '123', '234', '11');
-INSERT INTO `t_user` VALUES ('294a2957d53d48d4a677dc975297048d', null, '123', '234', '11');
-INSERT INTO `t_user` VALUES ('29913213551f47ac8849068ca6891e5e', null, '123', '5', '11');
-INSERT INTO `t_user` VALUES ('3', '盖伦', '123', '4', '11');
-INSERT INTO `t_user` VALUES ('3081a691b95f4a72a42361b27448704c', '盖伦啊啊啊1', '80082088201', '567', '11');
-INSERT INTO `t_user` VALUES ('4', '盖伦', '123', '678', '11');
-INSERT INTO `t_user` VALUES ('5', '123', '123', '678', '11');
-INSERT INTO `t_user` VALUES ('5335da102c3740a2849ea0f8d9d5b62b', null, '123', 'yu', '11');
-INSERT INTO `t_user` VALUES ('54a40868c50047ca8595425df751c5f3', null, '123', '6', '11');
-INSERT INTO `t_user` VALUES ('6', '342', '234', '45', '11');
-INSERT INTO `t_user` VALUES ('7', '2334', '23432', '234', '11');
-INSERT INTO `t_user` VALUES ('70160576ed964ac5a97ba68432b3f8d8', null, '123', '23', '11');
-INSERT INTO `t_user` VALUES ('8', '盖伦', '123', '123', '11');
-INSERT INTO `t_user` VALUES ('8597d8a9bacd4c5aa122cfaeffb16801', '盖伦啊啊啊1', '80082088201', '12', '11');
-INSERT INTO `t_user` VALUES ('881809904d904839b250f65f938fa8d2', null, '123', '34', '11');
-INSERT INTO `t_user` VALUES ('a51d068e7ff14c8dae3149e0fde2023a', null, '123', '234', '11');
-INSERT INTO `t_user` VALUES ('a82e28f92b804996b8397412e93402e4', null, '123', '234', '11');
-INSERT INTO `t_user` VALUES ('abb55e9f2a6c4ba7910b81c3f12c6055', null, '123', '234', '11');
-INSERT INTO `t_user` VALUES ('b4a25a6a53f14ccfa691af464817db2b', null, '123', '234', '11');
-INSERT INTO `t_user` VALUES ('b5e84a6f473a4423a73378ece46381f8', null, '123', '3444', '11');
-INSERT INTO `t_user` VALUES ('d956ce029f7e4160a09c0f836fc45de8', null, '123', '23', '11');
+INSERT INTO `user` VALUES ('1c8798f464df4cdba31504db95ce4e9d', '盖伦啊啊啊1', '123', '18668437101', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('2', '盖伦', '123', '234', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('294a2957d53d48d4a677dc975297048d', null, '123', '234', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('29913213551f47ac8849068ca6891e5e', null, '123', '5', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('3', '盖伦', '123', '4', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('3081a691b95f4a72a42361b27448704c', '盖伦啊啊啊1', '80082088201', '567', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('4', '盖伦', '123', '678', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('5', '123', '123', '678', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('5335da102c3740a2849ea0f8d9d5b62b', null, '123', 'yu', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('54a40868c50047ca8595425df751c5f3', null, '123', '6', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('6', '342', '234', '45', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('7', '2334', '23432', '234', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('70160576ed964ac5a97ba68432b3f8d8', null, '123', '23', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('8', '盖伦', '123', '123', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('8597d8a9bacd4c5aa122cfaeffb16801', '盖伦啊啊啊1', '80082088201', '12', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('881809904d904839b250f65f938fa8d2', null, '123', '34', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('a51d068e7ff14c8dae3149e0fde2023a', null, '123', '234', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('a82e28f92b804996b8397412e93402e4', null, '123', '234', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('abb55e9f2a6c4ba7910b81c3f12c6055', null, '123', '234', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('b4a25a6a53f14ccfa691af464817db2b', null, '123', '234', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('b5e84a6f473a4423a73378ece46381f8', null, '123', '3444', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('d956ce029f7e4160a09c0f836fc45de8', null, '123', '23', '11', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `id` varchar(50) NOT NULL,
+  `roleid` varchar(255) DEFAULT NULL,
+  `userid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES ('1', '1', '1c8798f464df4cdba31504db95ce4e9d');
